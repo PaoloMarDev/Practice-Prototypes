5fff57798f71968bbf3488c1c58dc684b2d04f5b
PaoloMar <PaoloMar_@hotmail.com>
2025-08-17 14:27:39 -0600

    Comentamos los códigos

--- P1_mousetrailanimation/index.js
+++ P1_mousetrailanimation/index.js
@@ -1,19 +1,24 @@
+
 const bodyEl = document.querySelector("body");
 
-bodyEl.addEventListener("mousemove", (event) => {
-    const xPos = event.offsetX;
+bodyEl.addEventListener("mousemove", (event) => { // Funcion de flecha
+    // Conseguimos la posición del mouse "x" e "y"
+    const xPos = event.offsetX;                   
     const yPos = event.offsetY;
     
+    // Creamos el elemento/etiqueta "span"
     const spanEl = document.createElement("span");
     
-    spanEl.style.left = xPos + "px"
+    // Le ponemos la posición del mouse al "span"
+    spanEl.style.left = xPos + "px" 
     spanEl.style.top = yPos + "px"
     
+    // Le asignamos al "body" el "span" como hijo
     bodyEl.appendChild(spanEl);
     
-    const size = Math.random() * 100
-    
-    spanEl.style.height = size + "px"
+    const size = Math.random() * 100 // generamos un numero random
+     
+    spanEl.style.height = size + "px" // le cambiamos el tamaño por un numero random
     spanEl.style.width = size + "px"
 
     // Removemos la etiqueta span despues de 3000 mls
